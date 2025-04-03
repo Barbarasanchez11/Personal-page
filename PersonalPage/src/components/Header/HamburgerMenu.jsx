312950afd82308fa7bf9d494b396c6aaaa96390b
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-03 17:21:14 +0200

    styles: improved transition on menu page

--- PersonalPage/src/components/Header/HamburgerMenu.jsx
+++ PersonalPage/src/components/Header/HamburgerMenu.jsx
@@ -1,56 +1,35 @@
-import { useState } from "react"
 import { Link } from "react-router-dom";
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-
+import { motion } from "framer-motion";
 import '../../styles/HamburgerMenu.css'
 
 const HamburgerMenu = () => {
-
-    const [isOpen,setIsOpen] = useState(false)
-    const [isHome,setIsHome] = useState(true)
-
-    const clickHandler = () => {
-      setIsOpen(!isOpen)
-      
-    }
-    
-    const links = [
-        
-        {name: 'Projects', path: '/projects', id: crypto.randomUUID},
-        {name: 'AboutMe', path: '/aboutme', id: crypto.randomUUID},
-        {name: 'HireMe', path: '/hireme', id: crypto.randomUUID},
-        {name: 'Home', path: '/', id: crypto.randomUUID}
-    ]
-    
     return (
-      <nav className="navbar">
-        <div>
-        Logo
-        </div>
-        <div  onClick={clickHandler}>
-          <MenuOutlinedIcon  />
-        </div>
-        <div className= {!isOpen ? "close-navbar" : 'close-navbar showNavbar'} onClick={clickHandler}>
-          <CloseOutlinedIcon />
-
-          {isOpen && (
-        <ul className="menu-list">
-          {links.map((link) => (
-            <li key={link.name} className="menu-item">
-              <Link to={link.path} className="menu-link">
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-        </div>
-        
-
-        
-      </nav>
-       
+        <nav className="navbar">
+            <motion.div 
+                className="logo"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.8, duration: 0.3 }}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+            >
+                BS
+            </motion.div>
+            
+            <Link to="/menu">
+                <motion.div 
+                    className="menu-icon"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 0.8, duration: 0.3 }}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                >
+                    <MenuOutlinedIcon />
+                </motion.div>
+            </Link>
+        </nav>
     )
 }
 
