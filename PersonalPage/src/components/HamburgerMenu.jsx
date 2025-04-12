57d27bdf31b817cd4bf2e40c85532525d6cfa36b
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-12 12:19:47 +0200

    feat:changes text english to spanish

--- PersonalPage/src/components/HamburgerMenu.jsx
+++ PersonalPage/src/components/HamburgerMenu.jsx
@@ -26,9 +26,9 @@ const HamburgerMenu = () => {
             <nav className={`slide-menu ${isOpen ? 'open' : ''}`}>
                 <div className="menu-content">
                     <Link to="/" onClick={toggleMenu}>Home</Link>
-                    <Link to="/about" onClick={toggleMenu}>About</Link>
-                    <Link to="/projects" onClick={toggleMenu}>Projects</Link>
-                    <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+                    <Link to="/about" onClick={toggleMenu}>Sobre mi</Link>
+                    <Link to="/projects" onClick={toggleMenu}>Proyectos</Link>
+                    <Link to="/contact" onClick={toggleMenu}>Contacto</Link>
                 </div>
             </nav>
         </>
--- PersonalPage/src/components/Navbar.jsx
+++ PersonalPage/src/components/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
             </div>
             <div className="nav-links">
                 <Link to="/" className="nav-link">Home</Link>
-                <Link to="/about" className="nav-link">About</Link>
-                <Link to="/projects" className="nav-link">Projects</Link>
-                <Link to="/contact" className="nav-link">Contact</Link>
+                <Link to="/about" className="nav-link">Sobre mi</Link>
+                <Link to="/projects" className="nav-link">Proyectos</Link>
+                <Link to="/contact" className="nav-link">Contacto</Link>
             </div>
         </nav>
     );
